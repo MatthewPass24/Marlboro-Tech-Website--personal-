64408dd48eefeb66a00698dfed0e1c5d25c5f58d
MatthewPass24 <matthewpass24@gmail.com>
2025-02-07 14:57:17 -0500

    finished website section

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+  $(document).ready(function() {
+
+    $(".image").hover(
+      function () {
+        $(this).find(".image__overlay").stop().fadeTo(500, 1); // Fade in overlay
+        $(this).find(".image__overlay > *").stop().animate({ top: "0px", opacity: 1 }, 500); // Move text up
+      },
+      function () {
+        $(this).find(".image__overlay").stop().fadeTo(500, 0); // Fade out overlay
+        $(this).find(".image__overlay > *").stop().animate({ top: "50px", opacity: 0 }, 500); // Move text down
+      }
+    );
+});
