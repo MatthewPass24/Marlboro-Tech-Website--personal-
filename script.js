0a57e6fd74459f8e0cf7b4c8410a413cc78534ff
MatthewPass24 <matthewpass24@gmail.com>
2025-02-08 23:06:16 -0500

    updated card carousel and added accordion on second page

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Next and previous navigation
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 2
     },
-  });
-  $(document).ready(function() {
-
-    $(".image").hover(
-      function () {
-        $(this).find(".image__overlay").stop().fadeTo(500, 1); // Fade in overlay
-        $(this).find(".image__overlay > *").stop().animate({ top: "0px", opacity: 1 }, 500); // Move text up
-      },
-      function () {
-        $(this).find(".image__overlay").stop().fadeTo(500, 0); // Fade out overlay
-        $(this).find(".image__overlay > *").stop().animate({ top: "50px", opacity: 0 }, 500); // Move text down
-      }
-    );
+    1024: {
+      slidesPerView: 3
+    }
+  }
 });
